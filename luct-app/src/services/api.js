f4e6254613130f4876d2b10dc0876953479dfae8
lehakoe268 <lehakoepanyane@outlook.com>
2025-10-12 23:51:46 +0200

    Update API URL for Render deployment

--- luct-app/src/services/api.js
+++ luct-app/src/services/api.js
@@ -1,7 +1,8 @@
 // services/api.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5001/api";
+const API_BASE_URL = "https://https://luct-reporting-backend-476s.onrender.com/api";
+
 
 // Create Axios instance
 const api = axios.create({
