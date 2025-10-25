0bedad7e81d8fca7bb14c37f30fa9189a06660d9
lehakoe268 <lehakoepanyane@outlook.com>
2025-10-25 05:00:37 +0200

    panyane

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003"],
+  origin: ['https://luct-reporting-front.onrender.com'],
   credentials: true
 }));
 
